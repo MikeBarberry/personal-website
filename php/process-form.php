1f8d38dbddfdd341cd4cd330dcfc833ec945580a
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2021-04-26 11:30:02 -0700

    change to aws sdk

--- php/process-form.php
+++ php/process-form.php
@@ -1,19 +1,74 @@
 <?php
-if (isset($_REQUEST['name'],$_REQUEST['email'])) {
-      
-    $name = $_REQUEST['name'];
-    $mail = $_REQUEST['email'];
-    $subject = $_REQUEST['subject'];
-    $message = $_REQUEST['message'];
 
-    // Set your email address where you want to receive emails.
-    $to = 'mikebarberry@protonmail.com';
+// If necessary, modify the path in the require statement below to refer to the 
+// location of your Composer autoload.php file.
+require 'vendor/autoload.php';
 
-    $headers = "From: ".$name." <".$mail."> \r\n";
+use Aws\Ses\SesClient;
+use Aws\Exception\AwsException;
 
-    $send_email = mail($to,$subject,$message,$headers);
+// Create an SesClient. Change the value of the region parameter if you're 
+// using an AWS Region other than US West (Oregon). Change the value of the
+// profile parameter if you want to use a profile in your credentials file
+// other than the default.
+$SesClient = new SesClient([
+    'profile' => 'default',
+    'version' => '2010-12-01',
+    'region'  => 'us-east-1'
+]);
 
-    echo ($send_email) ? 'success' : 'error';
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender_email = 'mail@mikebarberry.com';
 
+// Replace these sample addresses with the addresses of your recipients. If
+// your account is still in the sandbox, these addresses must be verified.
+$recipient_emails = ['mikebarberry@protonmail.com'];
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment the following variable, and the
+// 'ConfigurationSetName' => $configuration_set argument below.
+
+$subject = $_REQUEST['subject'];
+$plaintext_body = $_REQUEST['message'];
+$html_body =  '<h1>New Message from Personal Website</h1>'.
+              '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+              'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+              'AWS SDK for PHP</a>.</p>';
+$char_set = 'UTF-8';
+
+try {
+    $result = $SesClient->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $recipient_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+          'Body' => [
+              'Html' => [
+                  'Charset' => $char_set,
+                  'Data' => $html_body,
+              ],
+              'Text' => [
+                  'Charset' => $char_set,
+                  'Data' => $plaintext_body,
+              ],
+          ],
+          'Subject' => [
+              'Charset' => $char_set,
+              'Data' => $subject,
+          ],
+        ],
+        // If you aren't using a configuration set, comment or delete the
+        // following line
+    ]);
+    $messageId = $result['MessageId'];
+    echo("Email sent! Message ID: $messageId"."\n");
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    echo "\n";
 }
-?>
+    
